8acb93bee355feda27daa36cb2de8ba8ee5e882c
Akshata72 <sableakshata880@gmail.com>
2022-03-13 11:02:59 +0530

    Added:Factors Logic..

---
+++ Factors/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Factors
+{
+    class Factors
+    {
+
+        static void Main(string[]args)
+        {
+            int Count = 0; 
+            Console.WriteLine("Enter a Number:");
+            int Num = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= Num/2;  i++)
+            {
+                if (Num % i == 0)
+                {
+                    Console.WriteLine("Factors :" +i);
+                    Count++;
+                }
+                
+
+
+            }
+            Console.WriteLine("Number Of Factors:" +Count);
+        }
+
+    }
+
+}
